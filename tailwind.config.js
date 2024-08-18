dc201a728f9a5c02e5c66d6fd13bbb91d4a0c3ee
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-08-18 14:53:29 +0300

    Добавил в tailwind конфиг цвета и брейкпоинты

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,32 @@
 module.exports = {
   content: ['./src/**/*.{html,vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '25rem',
+        tablet: '50rem',
+        desktop: '80rem',
+      },
+      container: {
+        center: true,
+        padding: 'clamp(1rem, 5vw, 1.5rem)',
+        screens: {
+          desktop: '80rem',
+        },
+      },
+      colors: {
+        white: '#fff',
+        black: '#000',
+        blue: {
+          primary: '#2884EF',
+          gray: {
+            100: '#EEF6FF',
+            200: '#D8D8E5',
+            300: '#A1A5B7',
+            400: '#7E8299',
+          },
+        },
+      },
+    },
   },
 };
