51f301f4b3c6fa7355a0730be13f2dc5c6d705c1
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-08-19 15:42:54 +0300

    Добавил безопасные отсутпу для списка постов

--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -14,70 +14,72 @@ defineProps({
 </script>
 
 <template>
-  <section class="container bg-white my-5 p-5 rounded-xl">
-    <ul v-if="items.length" class="list">
-      <li
-        class="flex flex-col gap-2.5"
-        v-for="item in items"
-        :key="item"
-      >
-        <button
-          class="rounded-xl overflow-hidden"
-          style="aspect-ratio: 400 / 250"
-          @click="emit('toggleModal', { open: true, item: item })"
+  <section class="container">
+    <div class="bg-white my-5 p-5 rounded-xl">
+      <ul v-if="items.length" class="list">
+        <li
+          class="flex flex-col gap-2.5"
+          v-for="item in items"
+          :key="item"
         >
-          <ImgTemplate
-            class=" object-cover object-center"
-            :src="item.image"
-          />
-        </button>
-        <PostInfo :item="item" />
-        <header class="text-large font-semibold">
-          {{ item.title }}
-        </header>
-        <p class="text-normal">
-          {{ item.description.short }}
-        </p>
-        <ul class="flex gap-2.5 text-sm">
-          <li
-            v-for="category in item.categories"
-            :key="category"
-            class="
+          <button
+            class="rounded-xl overflow-hidden"
+            style="aspect-ratio: 400 / 250"
+            @click="emit('toggleModal', { open: true, item: item })"
+          >
+            <ImgTemplate
+              class=" object-cover object-center"
+              :src="item.image"
+            />
+          </button>
+          <PostInfo :item="item" />
+          <header class="text-large font-semibold">
+            {{ item.title }}
+          </header>
+          <p class="text-normal">
+            {{ item.description.short }}
+          </p>
+          <ul class="flex gap-2.5 text-sm">
+            <li
+              v-for="category in item.categories"
+              :key="category"
+              class="
               text-blue-primary
               bg-blue-gray-100
               capitalize
               rounded-3xl
               px-3 py-1.5
             "
-          >
-            {{ category }}
-          </li>
-        </ul>
-      </li>
-    </ul>
-    <div
-      v-else class="
+            >
+              {{ category }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div
+        v-else class="
         max-w-[20rem] mx-auto
         text-center font-medium
         flex flex-col items-center gap-2
       "
-    >
-      <SvgTemplate
-        icon-id="search-empty" viewBox="0 0 34 42"
-        class="h-10 text-blue-gray-200"
-      />
-      <header class="text-normal text-blue-gray-400">
-        Поиск не дал результатов
-      </header>
-      <p class="text-small text-blue-gray-300">
-        Повторите поиск или используйте фильтр для структуризации контента
-      </p>
-      <button
-        class="text-blue-primary bg-transparent text-normal capitalize"
-        @click="emit('changeFilter', { search: '', category: [] })"
       >
-        сбросить
-      </button>
+        <SvgTemplate
+          icon-id="search-empty" viewBox="0 0 34 42"
+          class="h-10 text-blue-gray-200"
+        />
+        <header class="text-normal text-blue-gray-400">
+          Поиск не дал результатов
+        </header>
+        <p class="text-small text-blue-gray-300">
+          Повторите поиск или используйте фильтр для структуризации контента
+        </p>
+        <button
+          class="text-blue-primary bg-transparent text-normal capitalize"
+          @click="emit('changeFilter', { search: '', category: [] })"
+        >
+          сбросить
+        </button>
+      </div>
     </div>
   </section>
 </template>
